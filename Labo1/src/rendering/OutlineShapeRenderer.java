283916c0ec713d46f08fd15b64139c97509043c5
Valentin Kaelin <vkaelin@hey.com>
2022-03-16 13:16:51 +0100

    refactor: only instanciate one stroke

--- Labo1/src/rendering/OutlineShapeRenderer.java
+++ Labo1/src/rendering/OutlineShapeRenderer.java
@@ -6,10 +6,11 @@ import java.awt.*;
 
 public class OutlineShapeRenderer implements Renderer {
     private static final int STROKE_WIDTH = 2;
+    private static final Stroke stroke = new BasicStroke(STROKE_WIDTH);
     
     @Override
     public void display(Graphics2D g, Bouncable b) {
-        g.setStroke(new BasicStroke(STROKE_WIDTH));
+        g.setStroke(stroke);
         g.setColor(b.getColor());
         g.draw(b.getShape());
     }
