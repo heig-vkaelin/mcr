e80297be208f5800926f4c500722e094ebaaa002
Valentin Kaelin <vkaelin@hey.com>
2022-03-16 10:52:15 +0100

    refactor: update image size when frame resize

--- Labo1/src/display/BouncerDisplayer.java
+++ Labo1/src/display/BouncerDisplayer.java
@@ -2,13 +2,14 @@ package display;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
 import java.awt.event.KeyAdapter;
-import java.awt.image.BufferedImage;
 
 public class BouncerDisplayer implements Displayer {
     private final JPanel panel;
     private final JFrame frame;
-    private BufferedImage image;
+    private Image image;
     
     private static BouncerDisplayer instance;
     private static final int INITIAL_WIDTH = 640;
@@ -25,13 +26,25 @@ public class BouncerDisplayer implements Displayer {
         
         frame.setVisible(true);
         
-        image = (BufferedImage) panel.createImage(getWidth(), getHeight());
+        image = createImage();
+        
+        // Mise à jour de la taille de l'image au resize
+        frame.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                image = createImage();
+            }
+        });
     }
     
     public static BouncerDisplayer getInstance() {
         if (instance == null)
             instance = new BouncerDisplayer();
         return instance;
+    }
+    
+    private Image createImage() {
+        return panel.createImage(getWidth(), getHeight());
     }
     
     @Override
@@ -51,15 +64,12 @@ public class BouncerDisplayer implements Displayer {
     
     @Override
     public void repaint() {
-        Graphics2D g = (Graphics2D) panel.getGraphics();
-        g.setBackground(Color.RED);
-        g.drawImage(image, 0, 0, Color.RED, null);
+        panel.getGraphics().drawImage(image, 0, 0, null);
         
         // Remise à 0 du contenu affiché
-//        Graphics2D g = getGraphics();
-//        g.clearRect(0, 0, getWidth(), getHeight());
-//        g.dispose();
-        image = (BufferedImage) panel.createImage(getWidth(), getHeight());
+        Graphics2D g = getGraphics();
+        g.clearRect(0, 0, getWidth(), getHeight());
+        g.dispose();
     }
     
     @Override
