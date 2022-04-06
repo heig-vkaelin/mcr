dba11d69325bded6e96827969b910dceeca9ea00
Valentin Kaelin <vkaelin@hey.com>
2022-04-06 11:03:26 +0200

    refactor: store label in window class

--- Labo2/src/display/DetailsClientWindow.java
+++ Labo2/src/display/DetailsClientWindow.java
@@ -10,6 +10,12 @@ public class DetailsClientWindow {
     private static final int INITIAL_HEIGHT = 250;
     
     private final JFrame frame;
+    private JLabel lbLastNameContent;
+    private JLabel lbFirstNameContent;
+    private JLabel lbCreditsContent;
+    private JLabel lbMilesContent;
+    private JLabel lbStatusContent;
+    private JLabel lbLastActionContent;
     
     public DetailsClientWindow(Account account) {
         frame = new JFrame();
@@ -24,42 +30,42 @@ public class DetailsClientWindow {
         
         JPanel lastNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbLastName = new JLabel("Last name :");
-        JLabel lbLastNameContent = new JLabel(account.getLastName());
+        lbLastNameContent = new JLabel(account.getLastName());
         lastNamePanel.add(lbLastName);
         lastNamePanel.add(lbLastNameContent);
         mainPanel.add(lastNamePanel);
         
         JPanel firstNamePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbFirstName = new JLabel("First name :");
-        JLabel lbFirstNameContent = new JLabel(account.getFirstName());
+        lbFirstNameContent = new JLabel(account.getFirstName());
         firstNamePanel.add(lbFirstName);
         firstNamePanel.add(lbFirstNameContent);
         mainPanel.add(firstNamePanel);
         
         JPanel creditsPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbCredits = new JLabel("Credits :");
-        JLabel lbCreditsContent = new JLabel(String.valueOf(account.getState().getBalance()));
+        lbCreditsContent = new JLabel(String.valueOf(account.getState().getBalance()));
         creditsPanel.add(lbCredits);
         creditsPanel.add(lbCreditsContent);
         mainPanel.add(creditsPanel);
         
         JPanel milesPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbMiles = new JLabel("Nb miles :");
-        JLabel lbMilesContent = new JLabel(String.valueOf(account.getState().getMiles()));
+        lbMilesContent = new JLabel(String.valueOf(account.getState().getMiles()));
         milesPanel.add(lbMiles);
         milesPanel.add(lbMilesContent);
         mainPanel.add(milesPanel);
         
         JPanel statusPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbStatus = new JLabel("Status :");
-        JLabel lbStatusContent = new JLabel(String.valueOf(account.getState().toString()));
+        lbStatusContent = new JLabel(String.valueOf(account.getState().toString()));
         statusPanel.add(lbStatus);
         statusPanel.add(lbStatusContent);
         mainPanel.add(statusPanel);
         
         JPanel lastActionPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         JLabel lbLastAction = new JLabel("Last action :");
-        JLabel lbLastActionContent = new JLabel("");
+        lbLastActionContent = new JLabel("");
         lastActionPanel.add(lbLastAction);
         lastActionPanel.add(lbLastActionContent);
         mainPanel.add(lastActionPanel);
--- Labo2/src/display/StatusClientWindow.java
+++ Labo2/src/display/StatusClientWindow.java
@@ -4,18 +4,22 @@ import account.Account;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.HashMap;
 
 public class StatusClientWindow {
     private static final int INITIAL_WIDTH = 500;
     private static final int INITIAL_HEIGHT = 250;
     
     private final JFrame frame;
+    private final HashMap<Integer, JLabel> labels;
     
     public StatusClientWindow(Account[] accounts) {
         frame = new JFrame();
         frame.setSize(INITIAL_WIDTH, INITIAL_HEIGHT);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         frame.setTitle("Statuses");
+        
+        labels = new HashMap<>();
         
         JPanel mainPanel = new JPanel();
         mainPanel.setBackground(Color.WHITE);
@@ -25,6 +29,7 @@ public class StatusClientWindow {
             JLabel lbStatus = new JLabel(account.info());
             lbStatus.setForeground(account.getState().getColor());
             mainPanel.add(lbStatus);
+            labels.put(account.getId(), lbStatus);
         }
         
         frame.setContentPane(mainPanel);
